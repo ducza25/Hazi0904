58a1af8950bd8ea27964d890fb636133ae8fc2f1
adrienn <adri@v.com>
2023-09-05 09:48:56 +0200

    ket szovegre katt ablak, nyilbol kez ha ravisszuk a szovre

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //console.log("hello")
 //console.error
+//onclick="alert('Aludni akarok')"
+
+document.querySelector('h1').onclick = function () {
+  alert('Aludni akarok!')
+}
+
+document.querySelector('h2').onclick = function () {
+  alert('Hello!')
+}
