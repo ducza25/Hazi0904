6c5062be8790adbbf96fb94c16429564932c2346
adri <adri@d.com>
2023-09-12 08:40:45 +0200

    feladat folyt

--- script.js
+++ script.js
@@ -10,4 +10,13 @@ document.querySelector('h2').onclick = function () {
   alert('Hello!')
 }*/
 
-const helloh1 = document.querySelector('hello')
+const helloh1 = document.querySelector('#hello')
+
+const container = document.querySelector('#container')
+
+helloh1.addEventListener('click', () => {
+  let card = document.createElement('div')
+  card.innerText = Math.floor(Math.random() * 6) + 1
+  container.append(card)
+  card.classList.add('card')
+})
